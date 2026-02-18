e3ad302039a37d8df00f1d1a6a3576d33030f28d
TenengFaith <muluhtenengfaithjunior@gmail.com>
2026-02-18 12:25:47 +0100

    data_types and loops exercises complete

---
+++ rust-exercises/students/TenengFaith/part1/03_loops/solutions.rs
@@ -0,0 +1,41 @@
+fn main(){
+    exercise_1();
+    exercise_2();
+}
+
+fn exercise_1(){
+    for i in 1..31{
+        if i%3==0 && i%5==0{
+            println!("FizzBuzz");
+        }
+        else if i%3==0{
+            println!("Fizz");
+        }
+        else if i%5==0{
+            println!("Buzz");
+        }
+        else{
+            println!("{}",1);
+        }
+    }
+}
+
+fn exercise_2(){
+    let mut counter=2;
+    let result= loop{
+        counter+=1;
+        if counter%7==0{
+            let sqrt= (counter as f64).sqrt();
+            if sqrt== sqrt.floor(){
+            break counter;
+
+            }
+        }
+    };
+
+    println!("The first perfect square divisible by 7 is: {}", result);
+}
+
+// fn exercise_3(){
+
+// }
